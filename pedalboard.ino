286cab67a46011489b75f27e03d2034bcd1141a1
Alexandre d'Alton <alex@alexdalton.org>
2013-09-11 16:05:57 +0200

    Refactor code.

--- pedalboard.ino
+++ pedalboard.ino
@@ -1,82 +1,13 @@
+/**
+ * Code for the custom made MIDI pedalBoard from Alex.
+ *
+ */
 #include <Wire.h>
 #include <Deuligne.h>
 #include <Keypad.h>
 #include <EEPROM.h>
 
-const byte ROWS = 3; //four rows
-const byte COLS = 5; //three columns
-char keys[ROWS][COLS] = {
-  {1,2,3,4,5},
-  {6,7,8,9,10},
-  {11,12,13,14, 15},
-};
-byte rowPins[ROWS] = {4, 3, 2}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {13, 12, 11, 10, 9}; //connect to the column pinouts of the keypad
 
-Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
-static Deuligne lcd;
-
-struct cc_data {
-  int key;
-  int number;
-  char type;
-} cc_data;
-
-struct cc_data buttons_config[15];
-
-void load_data(int key) {
-  Serial.print("Load data for: "); Serial.print(key);
-  Serial.print(EEPROM.read(key*2), HEX);
-  Serial.print(" ");
-  Serial.println(EEPROM.read(key*2+1), HEX);
-
-  cc_data.key = key;
-  cc_data.type = EEPROM.read(key * 2);  
-  if (cc_data.type != 'P' && cc_data.type != 'C')
-      cc_data.type = 'P';
-  cc_data.number = EEPROM.read(key*2+1);
-  if (cc_data.number > 127 || cc_data.number < 0) cc_data.number = 0;
-}
-
-void save_data(int key) {
-  Serial.print("Save data for: "); Serial.print(key);
-  Serial.print(cc_data.type, HEX);
-  Serial.print(" ");
-  Serial.println(cc_data.number, HEX);
-
-  EEPROM.write(key*2, cc_data.type);
-  EEPROM.write(key*2+1, cc_data.number);
-  buttons_config[key - 1] = cc_data;
-}
-
-void setup(){
-  Wire.begin(); // join i2c
-  lcd.init(); // LCD init
-
-  lcd.clear(); // Clear Display
-
-  lcd.backLight(true); // Backlight ON
-
-  lcd.setCursor(0,0); // Place cursor row 6, 1st line (counting from 0)
-  lcd.print("Pedalboard 1.0");
-  delay(1000);
-
-  Serial.begin(9600);
-
-  for (int i =1; i < 16; i++) {
-    load_data(i);
-    buttons_config[i-1] = cc_data;
-  }
-  lcd.clear();
-}
-  
-static char tmp[16];
-
-
-/**
- * Code for the custom made MIDI pedalBoard from Alex.
- *
- */
 
 #define MENU_ENTER      0xA0
 #define MENU_EXIT       0xA1
@@ -88,11 +19,104 @@ static char tmp[16];
 #define KEY_RIGHT       0x80
 #define KEY_ENTER       0x84
 
+
+/** display buffer. */  
+static char dispbuf[16];
+
+
+/* KEYPAD SECTION */
+const byte ROWS = 3; //three rows
+const byte COLS = 5; //five columns
+char keys[ROWS][COLS] = {
+  {1,2,3,4,5},
+  {6,7,8,9,10},
+  {11,12,13,14, 15},
+};
+byte rowPins[ROWS] = {4, 3, 2}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {13, 12, 11, 10, 9}; //connect to the column pinouts of the keypad
+
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+static Deuligne lcd;
+
+/** Switch configuration data */
+struct cc_data {
+  int key;
+  int number;
+  char type;
+} cc_data;
+
+struct cc_data buttons_config[15];
+
+static char * menu_items[] = {
+    "Midi Chnl",
+    "Brightness"
+};
+#define MENU_ITEM_COUNT (sizeof(menu_items) / sizeof(char *))
+
+
+#define STATE_RUN 0
+#define STATE_MENU 1
+
+int patch = 0;
+int bank = 0;
+
+uint16_t leds_state;
+int (*current_menu)(int) = NULL;
+
+
+
+void load_data(int key) {
+#ifdef DEBUG
+  Serial.print("Load data for: "); Serial.print(key);
+  Serial.print(EEPROM.read(key*2), HEX);
+  Serial.print(" ");
+  Serial.println(EEPROM.read(key*2+1), HEX);
+#endif
+  cc_data.key = key;
+  cc_data.type = EEPROM.read(key * 2);  
+  if (cc_data.type != 'P' && cc_data.type != 'C')
+      cc_data.type = 'P';
+  cc_data.number = EEPROM.read(key*2+1);
+  if (cc_data.number > 127 || cc_data.number < 0) cc_data.number = 0;
+}
+
+void save_data(int key) {
+#ifdef DEBUG
+  Serial.print("Save data for: "); Serial.print(key);
+  Serial.print(cc_data.type, HEX);
+  Serial.print(" ");
+  Serial.println(cc_data.number, HEX);
+#endif
+  EEPROM.write(key*2, cc_data.type);
+  EEPROM.write(key*2+1, cc_data.number);
+  buttons_config[key - 1] = cc_data;
+}
+
+void setup(){
+  Wire.begin();
+  lcd.init();
+
+  lcd.clear(); 
+  lcd.backLight(true);
+  lcd.setCursor(0,0);
+  lcd.print("Pedalboard 1.0");
+  delay(1000);
+
+  Serial.begin(9600); //Change this for 
+
+  for (int i =1; i < 16; i++) {
+    load_data(i);
+    buttons_config[i-1] = cc_data;
+  }
+  lcd.clear();
+}
+
 int select_cc_menu(int key) 
 {
     struct cc_data * data = &cc_data;
 
-    memset(tmp, ' ', 16);
+    memset(dispbuf, ' ', 16);
     switch(key) {
     case MENU_ENTER:
         /** Init data */
@@ -118,19 +142,17 @@ int select_cc_menu(int key)
         if (key > 0 && key < 0x80) 
           load_data(key);
     }
-    sprintf(tmp, "%cC# %3d         ", data->type, data->number);
+    sprintf(dispbuf, "%cC# %3d         ", data->type, data->number);
     lcd.setCursor(0, 1);
-    lcd.print(tmp);
+    lcd.print(dispbuf);
     return MENU_CONTINUE;    
 }
-
-int selected_key = -1;
 
 int main_menu(int key) 
 {
     switch (key) {
     case MENU_ENTER:
-        selected_key = -1;
+        cc_data.key = -1;
         lcd.clear();
         lcd.setCursor(0,0);
         lcd.print("Select key      ");
@@ -138,40 +160,31 @@ int main_menu(int key)
 
     default:
         if (key < 0x80 && key > 0) {
-            selected_key = key;
+            cc_data.key = key;
             select_cc_menu(key);
         } else if ( selected_key != -1 ){
             int ret = select_cc_menu(key);
             if (ret == MENU_EXIT) return MENU_EXIT;
         }
         break;
-    }    
-    if (selected_key != -1) {
-        snprintf(tmp, 16, "Button: %2d", selected_key);
+    }
+    
+    if (cc_data.key != -1) {
+      snprintf(dispbuf, 16, "Button: %2d", selected_key);
     } else {
-      sprintf(tmp, "Button: ??");
+      sprintf(dispbuf, "Button: <Press>");
     }
     lcd.setCursor(0, 0);
-    lcd.print(tmp);
+    lcd.print(dispbuf);
 }
-
-static char * menu_items[] = {
-    "Midi Chnl",
-    "Brightness"
-};
-#define MENU_ITEM_COUNT (sizeof(menu_items) / sizeof(char *))
-
-int (*current_menu)(int) = NULL;
 
 int run_menu(int key) 
 {
-    if (current_menu == NULL)
-      current_menu = main_menu;
-    
-    return current_menu(key);
+  if (current_menu == NULL)
+    current_menu = main_menu;
+  
+  return current_menu(key);
 }
-
-uint16_t leds_state;
 
 int getKey() {
   static int8_t oldkey = -1;
@@ -209,16 +222,9 @@ struct midi_msg * midiRead() {
   return 0;
 }
 
-#define STATE_RUN 0
-#define STATE_MENU 1
-
-int patch = 0;
-int bank = 0;
-
 void loop() {
     struct midi_msg * midimsg;
     static int state = STATE_RUN;
-    char tmp[16];
     int key = getKey();    
 
       switch(state) {
@@ -231,14 +237,14 @@ void loop() {
           } else if ( key > 0 && key < 0x80 ){
             patch = key;
             lcd.setCursor(10, 0);
-            sprintf(tmp, "%cC:%3d", buttons_config[key-1].type, buttons_config[key-1].number);
-            lcd.print(tmp);
+            sprintf(dispbuf, "%cC:%3d", buttons_config[key-1].type, buttons_config[key-1].number);
+            lcd.print(dispbuf);
           }
           lcd.setCursor(0,0); 
           lcd.print("RUN ");
           lcd.setCursor(0,1);
-          snprintf(tmp, 16, "B %02d P %02d", bank, patch);
-          lcd.print(tmp);        
+          snprintf(dispbuf, 16, "B %02d P %02d", bank, patch);
+          lcd.print(dispbuf);        
           break;
        case STATE_MENU:
           if (MENU_EXIT == run_menu(key)) {
